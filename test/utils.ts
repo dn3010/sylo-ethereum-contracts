0d967637138274ee87be76ce9ee34a0e9bb5fa3e
John Carlo San Pedro <j.carlosanpedro@gmail.com>
2024-06-27 15:48:47 +1200

    implement tests for deposit

--- test/utils.ts
+++ test/utils.ts
@@ -171,6 +171,7 @@ export async function deployContracts(
     protocolTimeManager,
     registries,
     authorizedAccounts,
+    deposits,
     rewardsManager,
     ticketing,
     stakingOrchestrator,
